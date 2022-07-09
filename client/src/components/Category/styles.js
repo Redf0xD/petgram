746364adbdef453fa242cbebc1e44d55be842c5a
Redf0xD <ricardoprato36@gmail.com>
2022-07-09 18:51:45 -0400

    :art: mejor estructura, archivos innecesarios eliminados

---
+++ client/src/components/Category/styles.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components'
+
+export const Anchor = styled.a`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-decoration: none;
+  width: 75px;
+`
+export const Image = styled.img`
+  overflow: hidden;
+  width: 75px;
+  height: 75px;
+  object-fit: cover;
+  border-radius: 50%;
+  border: 1px solid #ddd;
+  box-shadow: 0 10px 14px rgba(0, 0, 0, 0.2);
+`
