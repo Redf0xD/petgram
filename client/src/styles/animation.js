a1c3bf158842da2763f72f8b15b813aa7fb2bcb9
Redf0xD <ricardoprato36@gmail.com>
2022-07-16 12:34:25 -0400

    :sparkles: Creando animaciones reutilizables

---
+++ client/src/styles/animation.js
@@ -0,0 +1,14 @@
+import { keyframes, css } from 'styled-components'
+const FadeInKeyframe = keyframes`
+  from {
+    filter: blur(5px);
+    opacity: 0;
+  }
+  to {
+    filter: blur(0);
+    opacity: 1;
+  }
+`
+export const FadeIn = ({ time = '1s', type = 'ease' } = {}) => css`
+  animation: ${time} ${FadeInKeyframe} ${type};
+`
