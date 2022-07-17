1d299e48a0367cf781d15c35dc02425e1074ae80
Redf0xD <ricardoprato36@gmail.com>
2022-07-16 22:40:40 -0400

    :sparkles: añadiendo animaciones al hacer scroll

--- client/src/components/ListOfCategory/styles.js
+++ client/src/components/ListOfCategory/styles.js
@@ -1,4 +1,5 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
+import { FadeIn } from '../../styles/animation'
 
 export const List = styled.ul`
   display: flex;
@@ -7,6 +8,25 @@ export const List = styled.ul`
   &::-webkit-scrollbar {
     display: none;
   }
+  ${(props) =>
+    props.fixed &&
+    css`
+       {
+        ${FadeIn({ time: '0.5s' })}
+        position: fixed;
+        top: -20px;
+        left: 0;
+        right: 0;
+        z-index: 1;
+        background-color: #fff;
+        border-radius: 60px;
+        box-shadow: 0 0 20px rgba(0, 0, 0, 0.3);
+        max-width: 400px;
+        padding: 8px;
+        margin: 0 auto;
+        transform: scale(0.6);
+      }
+    `}
 `
 
 export const Items = styled.li`
