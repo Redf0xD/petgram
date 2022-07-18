587270ee3e627eaaa1ab5ecf53febbd584ae9e88
Redf0xD <ricardoprato36@gmail.com>
2022-07-17 21:02:54 -0400

    :art: Añadiendo import de React a la cabecera  para solucionar errores de linter

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import { ListOfCategory } from './components/ListOfCategory'
 import { ListOfPhotoCard } from './components/ListOfPhotoCard'
 import { Logo } from './components/Logo'
