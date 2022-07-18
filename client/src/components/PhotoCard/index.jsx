9f784a3f2322ed76ad2448cb9ccf8792476452d4
Redf0xD <ricardoprato36@gmail.com>
2022-07-17 21:03:26 -0400

    :sparkles: Añadiendo lazyLoad a las tarjetas

--- client/src/components/PhotoCard/index.jsx
+++ client/src/components/PhotoCard/index.jsx
@@ -1,21 +1,27 @@
-import { Img, ImgWrapper, Button } from './styles'
+import React from 'react'
+import { Img, ImgWrapper, Button, Article } from './styles'
 import { MdFavoriteBorder } from 'react-icons/md'
-
+import { LazyLoad } from '../../customHooks/LazyLoad'
 const DEFAULT_IMAGE =
   'https://res.cloudinary.com/midudev/image/upload/w_150/v1555671700/category_cats.jpg'
 
 export const PhotoCard = ({ id, src = DEFAULT_IMAGE, likes = 0 }) => {
+  const { show, element } = LazyLoad()
   return (
-    <article>
-      <a href={`detail/${id}`}>
-        <ImgWrapper>
-          <Img src={src} />
-        </ImgWrapper>
-      </a>
-      <Button>
-        <MdFavoriteBorder size='32px' />
-        {likes} likes!
-      </Button>
-    </article>
+    <Article ref={element}>
+      {show && (
+        <>
+          <a href={`detail/${id}`}>
+            <ImgWrapper>
+              <Img src={src} />
+            </ImgWrapper>
+          </a>
+          <Button>
+            <MdFavoriteBorder size="32px" />
+            {likes} likes!
+          </Button>
+        </>
+      )}
+    </Article>
   )
 }
