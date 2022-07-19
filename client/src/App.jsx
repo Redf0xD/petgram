1cbc9abce94a2ef6c665bfc01032b58081ef5054
Redf0xD <ricardoprato36@gmail.com>
2022-07-19 15:06:00 -0400

    :sparkles: Agregando nuevas rutas

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import { Logo } from './components/Logo'
+import { NavBar } from './components/NavBar'
 import { Detail } from './pages/Detail'
+import { Favs } from './pages/Favs'
 import { Home } from './pages/Home'
+import { NotRegisteredUser } from './pages/NotRegisteredUser'
+import { User } from './pages/User'
 import { GlobalStyle } from './styles/GlobalStyles'
+import { UserContext } from './Context/User/UserContext'
 
 const App = () => {
+  const { isAuth } = useContext(UserContext)
   return (
     <>
       <GlobalStyle />
@@ -19,7 +25,16 @@ const App = () => {
         <Route path="detail">
           <Route path=":detailId" element={<Detail />} />
         </Route>
+        <Route
+          path="favs"
+          element={isAuth ? <Favs /> : <NotRegisteredUser />}
+        />
+        <Route
+          path="user"
+          element={isAuth ? <User /> : <NotRegisteredUser />}
+        />
       </Routes>
+      <NavBar />
     </>
   )
 }
