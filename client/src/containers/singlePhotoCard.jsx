42060f577398dc5603d337edf686c7cc6cb9ed68
Redf0xD <ricardoprato36@gmail.com>
2022-07-19 11:13:04 -0400

    :art: añadiendo valor por defecto a data y photo

--- client/src/containers/singlePhotoCard.jsx
+++ client/src/containers/singlePhotoCard.jsx
@@ -3,9 +3,9 @@ import { PhotoCard } from '../components/PhotoCard'
 import { useSinglePhoto } from '../customHooks/useSinglePhoto'
 
 export const SinglePhotoCard = ({ id }) => {
-  const { loading, error, data } = useSinglePhoto(id)
+  const { loading, error, data = []} = useSinglePhoto(id)
   if (error) return <p>Error :( </p>
 
-  const { photo } = data
+  const { photo = []} = data
   return <PhotoCard {...photo} loading={loading}/>
 }
