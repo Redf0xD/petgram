5d63e28f0fc72bb44d969a0252e8228481e9c025
Redf0xD <ricardoprato36@gmail.com>
2022-07-19 08:29:58 -0400

    :sparkles: Boton para mutar likes

---
+++ client/src/components/FavButton/styles.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+export const Button = styled.button`
+  appearance: none;
+  background: transparent;
+  border: 0;
+  outline: 0;
+  padding: 8px;
+  display: flex;
+  align-items: center;
+  & svg {
+    margin-right: 4px;
+  }
+`
