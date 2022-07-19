be8ea2bfcef9c2c9c0baee1853549af7ff00a4c2
Redf0xD <ricardoprato36@gmail.com>
2022-07-18 21:19:45 -0400

    :sparkles: Creando hook para almacenar valores en el localStorage

--- client/src/components/PhotoCard/index.jsx
+++ client/src/components/PhotoCard/index.jsx
@@ -2,26 +2,16 @@ import React from 'react'
 import { Img, ImgWrapper, Button, Article } from './styles'
 import { MdFavorite, MdFavoriteBorder } from 'react-icons/md'
 import { LazyLoad } from '../../customHooks/LazyLoad'
-import { useState } from 'react'
+import { useLocalStorage } from '../../customHooks/useLocalStorage'
 const DEFAULT_IMAGE =
   'https://res.cloudinary.com/midudev/image/upload/w_150/v1555671700/category_cats.jpg'
 
 export const PhotoCard = ({ id, src = DEFAULT_IMAGE, likes = 0 }) => {
   const { show, element } = LazyLoad()
   const key = `like-${id}`
-  const [liked, setLiked] = useState(() => {
-    try {
-      const like = window.localStorage.getItem(key)
-      return JSON.parse(like)
-    } catch {
-      return false
-    }
-  })
-  const setLocalStorage = value => {
-    window.localStorage.setItem(key, value)
-    setLiked(value)
-  }
+  const [liked, setLiked] = useLocalStorage(key, false)
   const Icon = liked ? MdFavorite : MdFavoriteBorder
+
   return (
     <Article ref={element}>
       {show && (
@@ -31,7 +21,7 @@ export const PhotoCard = ({ id, src = DEFAULT_IMAGE, likes = 0 }) => {
               <Img src={src} />
             </ImgWrapper>
           </a>
-          <Button onClick={() => setLocalStorage(!liked)}>
+          <Button onClick={() => setLiked(!liked)}>
             <Icon size="32px" />
             {likes} likes!
           </Button>
