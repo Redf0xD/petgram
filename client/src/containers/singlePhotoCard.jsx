d6cf6faf8038e141473effc55a4e90efd7022df2
Redf0xD <ricardoprato36@gmail.com>
2022-07-19 08:29:24 -0400

    :art: añadiendo prop para usar placeholder

--- client/src/containers/singlePhotoCard.jsx
+++ client/src/containers/singlePhotoCard.jsx
@@ -4,9 +4,8 @@ import { useSinglePhoto } from '../customHooks/useSinglePhoto'
 
 export const SinglePhotoCard = ({ id }) => {
   const { loading, error, data } = useSinglePhoto(id)
-  if (loading) return <p>Loading...</p>
   if (error) return <p>Error :( </p>
 
   const { photo } = data
-  return <PhotoCard {...photo} />
+  return <PhotoCard {...photo} loading={loading}/>
 }
