45e31087d043e09e212ee93f92eee8f6e7eaa111
Redf0xD <ricardoprato36@gmail.com>
2022-07-18 21:19:05 -0400

    :sparkles: Integrando graphql para mostrar fotos reales

--- client/src/components/ListOfPhotoCard/index.jsx
+++ client/src/components/ListOfPhotoCard/index.jsx
@@ -1,9 +1,15 @@
 import { PhotoCard } from '../PhotoCard'
+import { useQuery } from '@apollo/client'
+import {withPhotos} from '../../hoc/withPhotos'
 
-export const ListOfPhotoCard = () => {
+export const ListOfPhotoCard = ({categoryId}) => {
+  const {loading, error, data} = useQuery(withPhotos, {variables: {categoryId}})
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+  const {photos = []} = data
   return (
     <ul>
-      {[1, 2, 3, 4].map(key => <PhotoCard key={key} id={key}/>)}
+      {photos.map(photo => <PhotoCard key={photo.id} {...photo}/>)}
     </ul>
   )
 }
