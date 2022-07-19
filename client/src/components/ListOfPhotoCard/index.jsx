94c80c2a35a4a227ddb30bb0159ccbf6f72c4890
Redf0xD <ricardoprato36@gmail.com>
2022-07-19 08:27:04 -0400

    :bug: Añadiendo valores default a data y photos

--- client/src/components/ListOfPhotoCard/index.jsx
+++ client/src/components/ListOfPhotoCard/index.jsx
@@ -1,15 +1,15 @@
+import React from 'react'
 import { useWithPhotos } from '../../customHooks/usePhotos'
 import { PhotoCard } from '../PhotoCard'
 
 export const ListOfPhotoCard = ({ categoryId }) => {
-  const { loading, error, data } = useWithPhotos(categoryId)
-  if (loading) return <p>Loading...</p>
+  const { loading, error, data = []} = useWithPhotos(categoryId)
   if (error) return <p>Error :(</p>
-  const { photos } = data
+  const { photos = []} = data
   return (
     <ul>
       {photos?.map(photo => (
-        <PhotoCard key={photo.id} {...photo} />
+        <PhotoCard key={photo.id} {...photo} loading={loading}/>
       ))}
     </ul>
   )
