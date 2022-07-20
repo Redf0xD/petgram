b19094b13611410b167282d039b9e9b02caa3f0c
Redf0xD <ricardoprato36@gmail.com>
2022-07-20 13:11:30 -0400

    :sparkles: agregando render condicional

--- client/src/components/NavBar/index.jsx
+++ client/src/components/NavBar/index.jsx
@@ -1,17 +1,30 @@
+import { useContext } from 'react'
+import { useLocation } from 'react-router-dom'
 import { Nav, Link } from './styles'
 import { MdHome, MdFavoriteBorder, MdPersonOutline } from 'react-icons/md'
-const SIZE = "32px"
+import { UserContext } from '../../Context/User/UserContext'
+const SIZE = '32px'
 export const NavBar = () => {
+  const { isAuth } = useContext(UserContext)
+  const { pathname } = useLocation()
+  let path = '/user'
+
+  if (pathname === '/register') path = '/register'
+  else if (pathname === '/login') path = '/login'
+  else path = '/user'
+
   return (
     <Nav>
       <Link to={'/'}>
-        <MdHome size={SIZE}/>
+        <MdHome size={SIZE} />
       </Link>
-      <Link to="/favs">
-        <MdFavoriteBorder size={SIZE} />
-      </Link>
-      <Link to={'/user'}>
-        <MdPersonOutline size={SIZE}/>
+      {isAuth && (
+        <Link to="/favs">
+          <MdFavoriteBorder size={SIZE} />
+        </Link>
+      )}
+      <Link to={path}>
+        <MdPersonOutline size={SIZE} />
       </Link>
     </Nav>
   )
