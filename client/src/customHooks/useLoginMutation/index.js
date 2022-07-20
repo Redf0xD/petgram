ed5ef86f1a89fccfff73ae86cb56dc009026b911
Redf0xD <ricardoprato36@gmail.com>
2022-07-20 13:17:01 -0400

    :sparkles: hooks para login y registro

---
+++ client/src/customHooks/useLoginMutation/index.js
@@ -0,0 +1,11 @@
+import { useMutation, gql } from '@apollo/client'
+
+const LOGIN = gql`
+  mutation login($input: UserCredentials!) {
+    login(input: $input)
+  }
+`
+export const useLoginMutation = () => {
+  const [loginMutation, {error, loading}] = useMutation(LOGIN)
+  return { loginMutation, error, loading }
+}
---
+++ client/src/customHooks/useRegisterMutation/index.jsx
@@ -0,0 +1,11 @@
+import { useMutation, gql } from '@apollo/client'
+
+const REGISTER = gql`
+  mutation signup($input: UserCredentials!) {
+    signup(input: $input)
+  }
+`
+export const useRegisterMutation = () => {
+  const [registerMutation, {error, loading}] = useMutation(REGISTER)
+  return { registerMutation, error, loading }
+}
