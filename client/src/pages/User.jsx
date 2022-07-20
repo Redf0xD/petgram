4737aaffe5696eb7d788ad65515582a3574fbebd
Redf0xD <ricardoprato36@gmail.com>
2022-07-20 14:51:45 -0400

    :sparkles: log out

--- client/src/pages/User.jsx
+++ client/src/pages/User.jsx
@@ -1,3 +1,13 @@
+import { SubmitButton } from '../components/SubmitButton'
+import { UserContext } from '../Context/User/UserContext'
+import { useContext } from 'react'
+
 export const User = () => {
-  return <h1>User</h1>
+  const { desactiveAuth } = useContext(UserContext)
+  return (
+    <>
+      <h1>User</h1>
+      <SubmitButton onClick={desactiveAuth}>Log out</SubmitButton>
+    </>
+  )
 }
