96a4644d8797752cedfd05cd0f89730a27139a07
Redf0xD <ricardoprato36@gmail.com>
2022-07-20 14:55:30 -0400

    :sparkles: boton reutilizable para formularios

---
+++ client/src/components/SubmitButton/index.jsx
@@ -0,0 +1,8 @@
+import { Button } from './styles'
+export const SubmitButton = ({ disabled, children, onClick }) => {
+  return (
+    <Button disabled={disabled} onClick={onClick}>
+      {children}
+    </Button>
+  )
+}
---
+++ client/src/components/SubmitButton/styles.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export const Button = styled.button`
+  background: #a20a9e;
+  border-radius: 5px;
+  color: #fff;
+  height: 32px;
+  width: 100%;
+  text-align: center;
+  font-weight: bold;
+`
