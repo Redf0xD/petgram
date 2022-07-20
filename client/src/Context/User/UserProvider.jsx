ca50869ee8292aa95e222bab4e839d53de30c7e8
Redf0xD <ricardoprato36@gmail.com>
2022-07-20 14:52:18 -0400

    :sparkles: añadiendo metodo para log out

--- client/src/Context/User/UserProvider.jsx
+++ client/src/Context/User/UserProvider.jsx
@@ -1,26 +1,37 @@
 import React, { useReducer } from 'react'
 import { UserContext } from './UserContext.jsx'
 import { UserReducer } from './UserReducer.jsx'
+import { useApolloClient } from '@apollo/client'
 import { AUTH } from './types'
 
 export const UserProvider = ({ children }) => {
+  const client = useApolloClient()
   // Estado inicial
   const initialState = {
     isAuth: window.sessionStorage.getItem('token')
   }
   const [state, dispatch] = useReducer(UserReducer, initialState)
   ///funciones que modifican estados
-  const setAuth = token => {
+  const activeAuth = token => {
     dispatch({
       type: AUTH,
       payload: token
     })
     window.sessionStorage.setItem('token', token)
   }
-
+  const desactiveAuth = () => {
+    dispatch({
+      type: AUTH,
+      payload: null
+    })
+    window.sessionStorage.removeItem('token')
+    client.resetStore()
+  }
   return (
     <div>
-      <UserContext.Provider value={{ isAuth: state.isAuth, setAuth }}>
+      <UserContext.Provider
+        value={{ isAuth: state.isAuth, activeAuth, desactiveAuth }}
+      >
         {children}
       </UserContext.Provider>
     </div>
