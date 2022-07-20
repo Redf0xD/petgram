b426a5cf987a51df1cc7e9d476eeea85806d614b
Redf0xD <ricardoprato36@gmail.com>
2022-07-19 20:41:11 -0400

    :fire: eliminando codigo no usado

--- client/src/pages/NotRegisteredUser.jsx
+++ client/src/pages/NotRegisteredUser.jsx
@@ -1,15 +1,11 @@
-import { useContext } from 'react'
-import { UserContext } from '../Context/User/UserContext'
+import React  from 'react'
+import { UserForm } from '../components/UserForm'
 
 export const NotRegisteredUser = () => {
-  const { setAuth } = useContext(UserContext)
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    setAuth(true)
-  }
+
   return (
-    <form onSubmit={handleSubmit}>
-      <button>iniciar sesion</button>
-    </form>
+    <>
+      <UserForm />
+    </>
   )
 }
