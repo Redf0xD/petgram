65d6da8042b5d7d7cf3bec572ab15ea24821a702
Redf0xD <ricardoprato36@gmail.com>
2022-07-20 13:16:03 -0400

    :art: añadiendo manejo de peticiones y errores

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,48 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'
+import { UserContext } from './Context/User/UserContext'
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  ApolloLink,
+  from,
+  HttpLink
+} from '@apollo/client'
+import { onError } from '@apollo/client/link/error'
 import { HashRouter } from 'react-router-dom'
 import { UserProvider } from './Context/User/UserProvider'
 
+const authMiddleware = new ApolloLink((operation, forward) => {
+  const token = window.sessionStorage.getItem('token')
+  const authorization = token ? `Bearer ${token}` : ''
+  if (token) {
+    operation.setContext({
+      headers: {
+        authorization
+      }
+    })
+  }
+  return forward(operation)
+})
+
+const errorMiddleware = onError(({ networkError }) => {
+  if (networkError && networkError.result.code === 'invalid_token') {
+    window.sessionStorage.removeItem('token')
+    window.location = '/#/user'
+  }
+})
+
 const client = new ApolloClient({
-  uri: 'https://petgram-with-react.vercel.app/graphql',
-  cache: new InMemoryCache()
+  cache: new InMemoryCache(),
+  link: from([
+    errorMiddleware,
+    authMiddleware,
+    new HttpLink({
+      uri: 'https://petgram-with-react.vercel.app/graphql'
+    })
+  ])
 })
 
 ReactDOM.createRoot(document.getElementById('root')).render(
